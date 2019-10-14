1ee72e3eea005e29443d0fb3cb5c04210554ac59
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-14 10:48:38 +0300

    Add solutions

--- Exams/01_Exam -13 Oct 2019/Solutions/Section_03.sql
+++ Exams/01_Exam -13 Oct 2019/Solutions/Section_03.sql
@@ -35,6 +35,16 @@ SELECT
 	ORDER BY f.[Id], f.[Name], f.[Size]
 	
 --09 Most Contributed Repositories
+SELECT TOP(5)
+		[r].[Id],
+		[r].[Name],
+		COUNT([c].[Id]) AS [Commits]
+	FROM [dbo].[Repositories] AS r
+	JOIN [dbo].[RepositoriesContributors] AS [rc] ON [r].[Id] = [rc].[RepositoryId]
+	JOIN [dbo].[Users] AS [u] ON [rc].[ContributorId] = [u].[Id]
+	JOIN [dbo].[Commits] AS [c] ON [r].[Id] = [c].[RepositoryId]
+GROUP BY [r].[Id], [r].[Name]
+ORDER BY [Commits] DESC, [r].[Id], [r].[Name];
 
 --10 User and Files
 SELECT 
